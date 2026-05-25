06a0e4ca04c8b412d0cfd398a52e4d7a03eeee93
Azzam Alrashed <azzam-dev@Azzams-MacBook-Air.local>
2026-05-25 14:54:54 +0300

    Add Hasana iOS app

---
+++ Hasana/Hasana/HasanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HasanaApp.swift
+//  Hasana
+//
+//  Created by Azzam Alrashed on 25/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct HasanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
